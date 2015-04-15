cf0522298996229c9a696170105d11cc277f082b
Emir Hurem <emir_hurem@hotmail.com>
2015-04-15 11:12:55 +0200

    Functions for menu and valdiation
    Style editing, adding menu

--- skripta.js
+++ skripta.js
@@ -7,14 +7,47 @@ function validirajFormu() {
     }
 }
 
-function validirajTekst(polje){
-var greskaTekst = document.getElementById('greskaTekst');
+function samoSlova() { 
+var regex=/[^a-z|^A-Z|^\s]/;
+if(document.getElementById("ime").value.match(regex))
+{
+return false;
+}
+if(document.getElementById("prezime").value.match(regex))
+{
+return false;
+}  
+}
+  
+function validirajIme(){
+var greskaTekst = document.getElementById('greskaIme');
 greskaTekst.innerHTML="";
-	if(document.forms["forma"]["ime"].value == '' || document.forms["forma"]["ime"].length<3){
+	if(samoSlova()== false){
 		greskaTekst.style.display="inline-block";
 		greskaTekst.style.marginRight="50px";
 		greskaTekst.innerHTML+="<img src='emir logo1.png' id='error'>";
-		greskaTekst.innerHTML+="Ime nije validno!";
-		return false;
+		greskaTekst.innerHTML+="Slova i razmaci dozvoljeni!";
 	}
+}
+
+function validirajPrezime(){
+var greskaTekst = document.getElementById('greskaPrezime');
+greskaTekst.innerHTML="";
+	if(samoSlova()== false){
+		greskaTekst.style.display="inline-block";
+		greskaTekst.style.marginRight="50px";
+		greskaTekst.innerHTML+="<img src='emir logo1.png' id='error'>";
+		greskaTekst.innerHTML+="Slova i razmaci dozvoljeni!";
+	}
+}
+
+function otvori(id){
+	odabrani = document.getElementById(id);
+	odabrani.style.visibility="visible";
+}
+
+function zatvori(id){
+	odabrani = document.getElementById(id);
+	odabrani.style.visibility="hidden";
+
 }
